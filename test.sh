ea4208f48c6e05d420abb7bcf3b15a1db27c851b
Navid M <navidmomtahen@gmail.com>
2026-04-07 17:37:17 +0100

    add further tests

--- test.sh
+++ test.sh
@@ -2,3 +2,5 @@ cargo test
 ./test_stdlib.sh
 ./target/release/spectre self/parser.sx --test
 ./target/release/spectre self/lexer.sx --test
+./target/release/spectre self/codegen.sx --test
+./target/release/spectre self/sema.sx --test
