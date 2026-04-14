0b88ae57497616ac8a807d74759fd822a219e4aa
Navid M <navidmomtahen@gmail.com>
2026-04-15 00:02:16 +0100

    remove redundant test script

--- test.sh
+++
@@ -1,6 +0,0 @@
-cargo test
-./test_stdlib.sh
-./target/release/spectre self/parser.sx --test
-./target/release/spectre self/lexer.sx --test
-./target/release/spectre self/codegen.sx --test
-./target/release/spectre self/sema.sx --test
