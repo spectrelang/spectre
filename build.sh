1d223c9d8f4b015bb8c0b5a4733fb356c049391a
Navid M <navidmomtahen@gmail.com>
2026-04-16 19:19:21 +0100

    fix build script

--- build.sh
+++ build.sh
@@ -13,6 +13,7 @@ for arg in "$@"; do
 done
 
 cc -c std/csources/panic_handler.c -o std/csources/panic_handler.o || exit 1
+cc -c std/csources/yyjson_shim.c -o std/csources/yyjson_shim.o || exit 1
 
 echo "Building spectre-dev..."
 spectre ./src/sxc.sx -o spectre-dev || exit 1
