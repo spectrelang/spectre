b1b685e0d6d9f8bd90598b3c1f33e8b7df117738
Navid M <navidmomtahen@gmail.com>
2026-04-21 01:35:28 +0100

    add samples

---
+++ samples/csamples/hello10.c
@@ -0,0 +1,38 @@
+#ifndef M_IMAGE_H
+#define M_IMAGE_H
+
+#include <stdint.h>
+
+#define M_IMAGE_VERSION 1
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#ifndef MIAPI
+#define MIAPI extern
+#endif
+
+#define M_VOID   0
+#define M_BOOL   1
+#define M_BYTE   2
+#define M_UBYTE  3
+#define M_SHORT  4
+#define M_USHORT 5
+#define M_INT    6
+#define M_UINT   7
+#define M_HALF   8
+#define M_FLOAT  9
+#define M_DOUBLE 10
+
+struct m_image
+{
+   void *data;
+   int size;
+   int width;
+   int height;
+   int comp;
+   char type;
+};
+
+#define M_IMAGE_IDENTITY() {0, 0, 0, 0, 0, 0}
---
+++ samples/csamples/hello9.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int f() {
+    static int x = 0;
+    x++;
+    return x;
+}
+
+int main() {
+    printf("%d\n", f());
+    printf("%d\n", f());
+}
