918723f687f5812bac977c40abe4fef20cd30ac7
Navid M <navidmomtahen@gmail.com>
2026-05-06 19:24:50 +0100

    bootstrap: regenerate bootstrap files, add bootstrap all script

---
+++ bootstrap_all.sh
@@ -0,0 +1,6 @@
+make
+./spectre-dev ./src/sxc.sx --emit-ssa --src-target=linux > bootstrap/sxc.ssa
+./spectre-dev ./src/sxc.sx --emit-alt --src-target=linux > bootstrap/sxc.c
+./spectre-dev ./src/sxc.sx --emit-ssa --src-target=darwin > bootstrap/sxc_darwin.ssa
+./spectre-dev ./src/sxc.sx --emit-alt --src-target=darwin > bootstrap/sxc_darwin.c
+./spectre-dev ./src/sxc.sx --emit-alt --src-target=windows > bootstrap/sxcw.c
